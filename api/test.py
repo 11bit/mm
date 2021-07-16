94ec32d14794e2924860d5342893763357c2d66e
Ivan Buryak <buryak.ivan@yandex.ru>
2021-07-16 17:46:37 +0500

    test

---
+++ api/test.py
@@ -0,0 +1,20 @@
+from http.server import BaseHTTPRequestHandler
+from datetime import datetime
+import shutil
+from types import SimpleNamespace
+
+
+FILEPATH = 'input.ttf'
+
+class handler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    with open(FILEPATH, 'rb') as f:
+      self.send_response(200)
+      self.send_header("Content-Type", 'application/octet-stream')
+      self.send_header("Content-Disposition", 'attachment; filename="{}"'.format("output.ttf"))
+      self.send_header("Content-Length", str(fs.st_size))
+      self.end_headers()
+      shutil.copyfileobj(f, self.wfile)
+      # self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    return
