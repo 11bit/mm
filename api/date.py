2fa504d11df01d8373bd03af2e960e6b8efc3089
Ivan Buryak <buryak.ivan@yandex.ru>
2021-07-16 17:31:37 +0500

    try fonts

--- api/date.py
+++ api/date.py
@@ -1,11 +1,41 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+import shutil
+from opentype_feature_freezer import RemapByOTL
+from types import SimpleNamespace
+
+def convert():
+  args = {
+      'features': 'salt',
+      'usesuffix': 'SC',
+      "replacenames": "Charis SIL/Charix,CharisSIL/Charix",
+      "inpath": "input.ttf",
+      "outpath": "output.ttf",
+      'script': None,
+      'lang': None,
+      'report': None,
+      'names': None,
+      'suffix': None,
+      'info': None,
+      'zapnames': None,
+  }
+
+  p = RemapByOTL(SimpleNamespace(**args))
+  p.run()
+
+
+FILEPATH = 'output.ttf'
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    convert()
+    with open(FILEPATH, 'rb') as f:
+      self.send_response(200)
+      self.send_header("Content-Type", 'application/octet-stream')
+      self.send_header("Content-Disposition", 'attachment; filename="{}"'.format("output.ttf"))
+      self.send_header("Content-Length", str(fs.st_size))
+      self.end_headers()
+      shutil.copyfileobj(f, self.wfile)
+      # self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     return
